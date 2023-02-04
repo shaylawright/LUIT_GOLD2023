4d79789aee73ffeac60b298bfa86440a41be1aaf
shaylawright <124110782+shaylawright@users.noreply.github.com>
2023-02-04 10:44:46 -0500

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 echo "Please enter your name:"
-read name
+Shayla Wright
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && whoami 
+echo "Your username is:" Cloud_User
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on:" February 4, 2023
 echo "-----------------------------"
 sleep 2
-echo "My IP address is:"
+echo "My IP address is:" 54.214.227.214
 echo "-----------------------------"
 sleep 2
 curl ifconfig.me
